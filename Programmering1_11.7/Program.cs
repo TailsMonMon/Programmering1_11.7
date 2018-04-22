b20095eb65a785d084dce819b26dc65214bc1340
Monica Björk <m.bjork95@gmail.com>
2018-04-22 15:42:44 +0200

    Add project files.

---
+++ Programmering1_11.7/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programmering1_11._7 {
+    class Program {
+        static void Main(string[] args) {
+            Console.Write("Hur gammal är personen? ");
+            int age = Convert.ToInt32(Console.ReadLine());
+            OfAge(age);
+            Console.ReadLine();
+        }
+
+        //      CHECKS IF PERSON IS OF AGE
+        static string OfAge(int age) {
+            string str = "before";
+            if (age >= 18) {
+                str = "Personen är myndig.";
+            }
+            else {
+                str = "Personen är inte myndig.";
+            }
+            Console.WriteLine(str);
+            return str;
+        }
+    }
+}
